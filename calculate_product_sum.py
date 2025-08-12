c65de4c08477f8af102226d3c310c35230df7606
stephen mulwa <stephenmulwayoutube@outlook.com>
2025-08-12 04:54:16 -0700

    CAPITILIZIED I

--- calculate_product_sum.py
+++ calculate_product_sum.py
@@ -6,7 +6,7 @@ def calculate_product_sum(number_1,number_2):
         print(number_1+number_2)
 
 
-num_1=input("input num 1:")
-num_2=input("input num 2:")
+num_1=input("Input num 1:")
+num_2=input("Input num 2:")
 print("User entered Number 1: ",num_1," and Number 2: ",num_2)
 calculate_product_sum(int(num_1),int(num_2))
